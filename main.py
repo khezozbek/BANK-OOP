8595ed372adf38baa41437003541685d1ddd6e1e
Ezozbek Xolbutayev <141992374+khezozbek@users.noreply.github.com>
2023-10-13 17:06:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from money_transfer import transfer_funds
 account1 = register_account("123456789", "John Doe", 100.0)
 account2 = register_account("987654321", "Jane Smith", 50.0)
 
-# Perform money transfer
 transfer_funds(account1, account2, 75.0)
 
 # Get account balances
